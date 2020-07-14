200e28406135ff56ee72d6b3d8863f4b3693674a
souvick C <souvickdesign1@gmail.com>
2020-07-14 14:16:30 -0400

    error msg div added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
   app = express(),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server);
+usernames = [];
 
 server.listen(process.env.PORT || 3000);
 
@@ -14,8 +15,33 @@ app.get('/', function (req, res) {
 io.sockets.on('connection', function (socket) {
   console.log('Socket connected...');
 
+  socket.on('new user', function (data, callback) {
+    if (usernames.indexOf(data) != -1) {
+      calbback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      updateUsernames();
+    }
+  });
+
+  //update usernames
+  function updateUsernames() {
+    io.sockets.emit('username', username);
+  }
+
   //Send Message
   socket.on('send message', function (data) {
     io.sockets.emit('new message', { msg: data });
+  });
+
+  //Disconnect
+  socket.on('disconnect', function (data) {
+    if (!socket.username) {
+      return;
+    }
+    username.splice(usernames.indexOf(socket.username), 1);
+    updateUsernames();
   });
 });
